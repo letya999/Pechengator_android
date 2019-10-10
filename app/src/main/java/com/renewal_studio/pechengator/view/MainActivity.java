69a30697f5290f0f2c34007cd076ead692f734a1
letya999 <artemletyushev@gmail.com>
2019-10-10 22:18:14 +0300

    first commit

--- app/src/main/java/com/renewal_studio/pechengator/view/MainActivity.java
+++ app/src/main/java/com/renewal_studio/pechengator/view/MainActivity.java
@@ -11,13 +11,10 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-/*    @BindView(R.id.nav_host_fragment) - пример использования butterknife
-    Fragment fragment;*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-/*        ButterKnife.bind(this);*/
     }
 }
