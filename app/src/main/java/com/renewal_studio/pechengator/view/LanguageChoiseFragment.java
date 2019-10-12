92bb342a0c81f6b8ec4eafac05efdbacb8482a11
letya999 <artemletyushev@gmail.com>
2019-10-12 13:57:40 +0300

    add some window

--- app/src/main/java/com/renewal_studio/pechengator/view/LanguageChoiseFragment.java
+++ app/src/main/java/com/renewal_studio/pechengator/view/LanguageChoiseFragment.java
@@ -1,14 +1,28 @@
 package com.renewal_studio.pechengator.view;
 
+import android.Manifest;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
 import com.renewal_studio.pechengator.R;
 
+import java.util.List;
+
 public class LanguageChoiseFragment extends Fragment {
+
+    private View root;
 
     public LanguageChoiseFragment() {
 
@@ -17,7 +31,24 @@ public class LanguageChoiseFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_language_choise, container, false);
-    }
+        Dexter.withActivity(getActivity())
+                .withPermissions(
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.ACCESS_FINE_LOCATION)
+                .withListener(new MultiplePermissionsListener() {
+            @Override public void onPermissionsChecked(MultiplePermissionsReport report) {}
+            @Override public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {}
+        }).check();
 
+       root = inflater.inflate(R.layout.fragment_language_choise, container, false);
+       root.findViewById(R.id.lang_rus).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavController navController = Navigation.
+                        findNavController(getActivity(), R.id.nav_host_fragment);
+                navController.navigate(R.id.startFragment);
+            }
+        });
+        return root;
+    }
 }
--- app/src/main/java/com/renewal_studio/pechengator/view/StartFragment.java
+++ app/src/main/java/com/renewal_studio/pechengator/view/StartFragment.java
@@ -1,14 +1,21 @@
 package com.renewal_studio.pechengator.view;
 
+import android.app.Activity;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.AppLaunchChecker;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.renewal_studio.pechengator.R;
 
 public class StartFragment extends Fragment {
+
+    private View root;
 
     public StartFragment() {
 
@@ -17,7 +24,28 @@ public class StartFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_start, container, false);
+        if(!checkFirstStart(getActivity())) {
+            NavController navController = Navigation.
+                    findNavController(getActivity(), R.id.nav_host_fragment);
+            navController.navigate(R.id.languageChoiseFragment);
+        }
+        root = inflater.inflate(R.layout.fragment_start, container, false);
+        root.findViewById(R.id.btn_start).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavController navController = Navigation.
+                        findNavController(getActivity(), R.id.nav_host_fragment);
+                navController.navigate(R.id.listLocationFragment);
+            }
+        });
+        return root;
+    }
+
+    public static boolean checkFirstStart(Activity activity) {
+        if(AppLaunchChecker.hasStartedFromLauncher(activity))
+            return  true;
+        AppLaunchChecker.onActivityCreate(activity);
+        return  false;
     }
 
 }
