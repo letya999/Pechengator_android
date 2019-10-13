08977b93f335b7c25e0820d07b36304768477546
letya999 <artemletyushev@gmail.com>
2019-10-13 13:57:20 +0300

    add map

--- app/src/main/java/com/renewal_studio/pechengator/view/RouteFragment.java
+++ app/src/main/java/com/renewal_studio/pechengator/view/RouteFragment.java
@@ -1,26 +1,56 @@
 package com.renewal_studio.pechengator.view;
 
 
+import android.location.Location;
 import android.os.Bundle;
-
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Toast;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.renewal_studio.pechengator.R;
 
-public class RouteFragment extends Fragment {
+public class RouteFragment extends Fragment implements GoogleMap.OnMyLocationButtonClickListener,
+        GoogleMap.OnMyLocationClickListener,  OnMapReadyCallback {
+
+    View root;
+    private GoogleMap mMap;
+    SupportMapFragment mapFragment;
 
     public RouteFragment() {
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_route, container, false);
+        root = inflater.inflate(R.layout.fragment_route, container, false);
+        mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        return root;
     }
 
+    @Override
+    public void onMyLocationClick(@NonNull Location location) {
+        Toast.makeText(getContext(), "Current location:\n" + location, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        Toast.makeText(getContext(), "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+        return false;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        mMap.clear();
+        mMap.setMyLocationEnabled(true);
+        mMap.setOnMyLocationButtonClickListener(this);
+        mMap.setOnMyLocationClickListener(this);
+    }
 }
