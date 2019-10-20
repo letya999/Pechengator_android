e82131964a7c6818048c0064bb405ec005b4138d
letya999 <artemletyushev@gmail.com>
2019-10-20 20:53:59 +0300

    add adapter

--- app/src/main/java/com/renewal_studio/pechengator/support/DocumentQuote.java
+++ app/src/main/java/com/renewal_studio/pechengator/support/DocumentQuote.java
@@ -1,40 +1,138 @@
 package com.renewal_studio.pechengator.support;
 
+import android.os.Parcel;
+import android.os.Parcelable;
 import com.google.android.gms.maps.model.LatLng;
+import java.util.ArrayList;
+import java.util.List;
 
-public class DocumentQuote {
+public class DocumentQuote implements Parcelable {
 
-    private String devoted;
-    private LatLng geopoint;
     private String name;
     private String note;
-    private String[] photos;
-    private String visual_discription;
+    private String devoted;
+    private String visualDiscription;
+    private List<String> photos = new ArrayList<String>();
+    private LatLng geopoint;
 
-    public DocumentQuote() {};
+    public final static Parcelable.Creator<DocumentQuote> CREATOR = new Creator<DocumentQuote>() {
+        @SuppressWarnings({
+                "unchecked"
+        })
+        public DocumentQuote createFromParcel(Parcel in) {
+            return new DocumentQuote(in);
+        }
 
-    public String getDevoted(){
-        return devoted;
+        public DocumentQuote[] newArray(int size) {
+            return (new DocumentQuote[size]);
+        }
+
+    };
+
+    protected DocumentQuote(Parcel in) {
+        this.name = ((String) in.readValue((String.class.getClassLoader())));
+        this.note = ((String) in.readValue((String.class.getClassLoader())));
+        this.devoted = ((String) in.readValue((String.class.getClassLoader())));
+        this.visualDiscription = ((String) in.readValue((String.class.getClassLoader())));
+        in.readList(this.photos, (java.lang.String.class.getClassLoader()));
+        double lat = ((double) in.readValue((double.class.getClassLoader())));
+        double lng = ((double) in.readValue((double.class.getClassLoader())));
+        this.geopoint = new LatLng(lat,lng);
     }
 
-    public LatLng getGeopoint(){
-        return geopoint;
+    public DocumentQuote() {}
+
+    public DocumentQuote(String name, String note, String devoted,
+                         String visualDiscription, List<String> photos, LatLng geopoint) {
+        super();
+        this.name = name;
+        this.note = note;
+        this.devoted = devoted;
+        this.visualDiscription = visualDiscription;
+        this.photos = photos;
+        this.geopoint = geopoint;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public String getNote(){
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNote() {
         return note;
     }
 
-    public String[] getPhoto(){
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public String getDevoted() {
+        return devoted;
+    }
+
+    public void setDevoted(String devoted) {
+        this.devoted = devoted;
+    }
+
+    public String getVisualDiscription() {
+        return visualDiscription;
+    }
+
+    public void setVisualDiscription(String visualDiscription) {
+        this.visualDiscription = visualDiscription;
+    }
+
+    public List<String> getPhotos() {
         return photos;
     }
 
-    public String getVisual_discription(){
-        return visual_discription;
+    public void setPhotos(List<String> photos) {
+        this.photos = photos;
     }
 
+    public LatLng getGeopoint() {
+        return geopoint;
+    }
+
+    public void setGeopoint(LatLng geopoint) {
+        this.geopoint = geopoint;
+    }
+
+    @Override
+    public String toString() {
+        return new StringBuilder().append(name).append(" "+note).append(" "+devoted).
+                append(" "+visualDiscription).append(" "+photos).append(" "+geopoint.toString()).toString();
+    }
+
+    @Override
+    public int hashCode() {
+        return toString().hashCode();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this)
+            return true;
+        if ((other instanceof DocumentQuote) == false)
+            return false;
+        DocumentQuote rhs = ((DocumentQuote) other);
+        return rhs.equals(this);
+    }
+
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeValue(name);
+        dest.writeValue(note);
+        dest.writeValue(devoted);
+        dest.writeValue(visualDiscription);
+        dest.writeList(photos);
+        dest.writeValue(geopoint.latitude);
+        dest.writeValue(geopoint.longitude);
+    }
+
+    public int describeContents() {
+        return 0;
+    }
 }
--- app/src/main/java/com/renewal_studio/pechengator/view/ListLocationFragment.java
+++ app/src/main/java/com/renewal_studio/pechengator/view/ListLocationFragment.java
@@ -1,26 +1,41 @@
 package com.renewal_studio.pechengator.view;
 
-
+import android.content.Context;
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.renewal_studio.pechengator.R;
 import com.renewal_studio.pechengator.contract.ListLocationContract;
+import com.renewal_studio.pechengator.data.Location;
 import com.renewal_studio.pechengator.presenter.ListLocationPresenter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class ListLocationFragment extends Fragment implements ListLocationContract.View {
 
     private static final String TAG = "ListLocationFragment";
     private ListLocationContract.Presenter llPresenter;
 
-    public void ListLocationFragment() {
-    }
+    public void ListLocationFragment() {}
+
+    @BindView(R.id.list_location)
+    RecyclerView list_locations;
 
 
     @OnClick(R.id.searchTextView)
@@ -33,7 +48,61 @@ public class ListLocationFragment extends Fragment implements ListLocationContra
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_list_location, container, false);
+        View root = inflater.inflate(R.layout.fragment_list_location, container, false);
+        ButterKnife.bind(this, root);
+        list_locations.setAdapter(new ListLocationAdapter(getContext(), new ArrayList<Location>()));
+        list_locations.setLayoutManager(new LinearLayoutManager(getContext(), RecyclerView.VERTICAL, false));
+        return root;
+    }
+
+    public class ListLocationAdapter extends RecyclerView.Adapter<ListLocationAdapter.ItemViewHolder> {
+        private List<Location> locations;
+        private Context context;
+
+        public ListLocationAdapter(Context context, List<Location> locations){
+            this.context = context;
+            this.locations = locations;
+        }
+
+        @Override
+        public ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_location, parent, false);
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+            ItemViewHolder holder = new ItemViewHolder(view);
+            return holder;
+        }
+
+        @Override
+        public void onBindViewHolder(ItemViewHolder holder, int position) {
+            holder.name.setText(locations.get(position).getLocationName());
+            holder.desc.setText(locations.get(position).getLocationDiscription());
+        }
+
+        @Override
+        public int getItemCount() {
+            return locations.size();
+        }
+
+        class ItemViewHolder extends RecyclerView.ViewHolder{
+            @BindView(R.id.name)
+            TextView name;
+            @BindView(R.id.desc)
+            TextView desc;
+            @BindView(R.id.distance)
+            TextView distance;
+            @BindView(R.id.image)
+            ImageView image;
+
+            public ItemViewHolder(View view) {
+                super(view);
+                ButterKnife.bind(this, view);
+            }
+        }
     }
 
 }
